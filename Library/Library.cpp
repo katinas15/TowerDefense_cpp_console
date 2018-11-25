13e0f4aed474daf1a5455e4b565e38fda028f9d8
katinas15 <41838906+katinas15@users.noreply.github.com>
2018-11-25 12:53:27 +0200

    neveikia fullscreen, neveikia remove po set(bando istrinti sena langa and table), overflow kai table create su mouse click, neleidzia antro table sukurti????

--- Library/Library.cpp
+++ Library/Library.cpp
@@ -17,6 +17,8 @@ namespace lib {
 	int globalColor;
 	int x = -100;
 	int y = -100;
+	void nothing() {}
+
 	void clearscreen(int color) {	//istrinamas ekranas ir nustatoma global color
 		COORD coordScreen = { 0, 0 };
 		DWORD cCharsWritten;
@@ -122,11 +124,12 @@ namespace lib {
 		cout << text;
 	}
 
-	void printBMP(const char* filename, int x, int y) {
+	void printBMP(string file, int x, int y) {
 
 		//isveda bmp nuotrauka i konsole
 		//https://stackoverflow.com/questions/9296059/read-pixel-value-in-bmp-file
 		// read BMP file
+		const char* filename = file.c_str();
 		FILE* f = fopen(filename, "rb");
 
 		if (f == NULL)
@@ -253,11 +256,115 @@ namespace lib {
 	}
 };
 
-	void langas::fill(int color) {
-		lib::setColor(color);
-		int tempy = y;
+void langas::fill(int color) {
+	lib::setColor(color);
+	int tempy = y;
 
-		//uzspalvinama table vieta
+	//uzspalvinama table vieta
+	lib::setCursorPosition(x, tempy);
+	for (int i = 0; i <= height; i++) {
+		lib::setCursorPosition(x, tempy + i);
+		for (int j = 0; j <= width; j++) {
+			cout << " ";
+		}
+	}
+}
+
+void langas::create() {
+	remove();
+	int tempy = y;
+
+	fill(color);
+
+	char v, b, vk, vd, ak, ad;
+	if (borderType == 1) {//viengubas krastas
+		v = char(196);
+		b = char(179);
+		vk = char(218);
+		vd = char(191);
+		ak = char(192);
+		ad = char(217);
+	}
+	else if (borderType == 2) {//dvigubas krastas
+		v = char(205);
+		b = char(186);
+		vk = char(201);
+		vd = char(187);
+		ak = char(200);
+		ad = char(188);
+	}
+
+	if (borderType != 0) { // jei krastu nera toliau vnieko nereikia daryti
+		lib::setCursorPosition(x, y);
+		for (int i = 0; i <= width; i++) {	//virsus
+			cout << v;
+		}
+
+		tempy = y;
+		lib::setCursorPosition(x, tempy + height);
+		for (int i = 0; i < width; i++) {	//apacia
+			cout << v;
+		}
+
+		for (int i = tempy; i < tempy + height; i++) {	//kaire
+			lib::setCursorPosition(x, i);
+			cout << b;
+		}
+
+		for (int i = tempy; i < tempy + height; i++) {	//desine
+			lib::setCursorPosition(x + width, i);
+			cout << b;
+		}
+
+		tempy += height;
+
+		//kampai
+		lib::setCursorPosition(x, y);
+		cout << vk;
+		lib::setCursorPosition(x + width, y);
+		cout << vd;
+		lib::setCursorPosition(x, y + height);
+		cout << ak;
+		lib::setCursorPosition(x + width, y + height);
+		cout << ad;
+	}
+
+}
+
+void langas::remove() {
+	lib::setColor(lib::globalColor);
+	int tempy = y;
+
+	//uzspalvinama table vieta
+	lib::setCursorPosition(x, tempy);
+	for (int i = 0; i <= height; i++) {
+		lib::setCursorPosition(x, tempy + i);
+		for (int j = 0; j <= width; j++) {
+			cout << " ";
+		}
+	}
+}
+
+void langas::set(int sx, int sy, int swidth, int sheight, int scolor, int sborderType) {
+	/*istrinama sena lentele ir sukuriama nauja lentele su naujais parametrais*/
+	remove();
+	x = sx;
+	y = sy;
+	width = swidth;
+	height = sheight;
+	color = scolor;
+	borderType = sborderType;
+	create();
+}
+
+
+
+
+void table::fill(int color) {
+	lib::setColor(color);
+	int tempy = y;
+	//uzspalvinama table vieta/ uzprintinami tusti langeliai kur bus table
+	for (int r = 0; r < rows; r++) {
 		lib::setCursorPosition(x, tempy);
 		for (int i = 0; i <= height; i++) {
 			lib::setCursorPosition(x, tempy + i);
@@ -265,15 +372,138 @@ namespace lib {
 				cout << " ";
 			}
 		}
+		tempy += height;
+	}
+}
+
+void table::create() {	//sukuriama lentele
+	remove();
+	int tempy = y;
+	fill(color);
+
+	char v, b, vk, vd, ak, ad;
+	if (borderType == 0) {	//jei krastu nera tiesiog isvedamas tesktas
+		for (int r = 0; r < rows; r++) {
+			lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
+			cout << text[r];
+		}
+	}
+	else {
+		if (borderType == 1) {	//viengubas krastas
+			v = char(196);
+			b = char(179);
+			vk = char(218);
+			vd = char(191);
+			ak = char(192);
+			ad = char(217);
+		}
+		else if (borderType == 2) {	//dvigubas krastas
+			v = char(205);
+			b = char(186);
+			vk = char(201);
+			vd = char(187);
+			ak = char(200);
+			ad = char(188);
+		}
+
+
+		lib::setCursorPosition(x, y);
+		for (int i = 0; i <= width; i++) {	//virsus
+			cout << v;
+		}
+
+		tempy = y;
+		for (int r = 0; r < rows; r++) {
+			lib::setCursorPosition(x, tempy + height);
+			for (int i = 0; i <= width; i++) {	//apacia
+				cout << v;
+			}
+
+			for (int i = tempy; i <= tempy + height; i++) {	//kaire
+				lib::setCursorPosition(x, i);
+				cout << b;
+			}
+
+			for (int i = tempy; i <= tempy + height; i++) {	//desine
+				lib::setCursorPosition(x + width, i);
+				cout << b;
+			}
+
+			tempy += height;
+		}
+
+		//kampai
+		lib::setCursorPosition(x, y);
+		cout << vk;
+		lib::setCursorPosition(x + width, y);
+		cout << vd;
+		lib::setCursorPosition(x, y + height * rows);
+		cout << ak;
+		lib::setCursorPosition(x + width, y + height * rows);
+		cout << ad;
+
+		//tekstas
+		for (int r = 0; r < rows; r++) {
+			lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
+			cout << text[r];
+		}
+	}
+}
+
+void table::remove() {
+	lib::setColor(lib::globalColor);
+	int tempy = y;
+
+	//uzspalvinama table vieta
+	for (int r = 0; r < rows; r++) {
+		lib::setCursorPosition(x, tempy);
+		for (int i = 0; i <= height; i++) {
+			lib::setCursorPosition(x, tempy + i);
+			for (int j = 0; j <= width; j++) {
+				cout << " ";
+			}
+		}
+		tempy += height;
+	}
+}
+
+void table::set(int sx, int sy, int swidth, int sheight, int scolor, int srows, int sborderType) {
+	//istrinama sena lentele ir sukuriama nauja lentele su naujais parametrais
+	remove();
+	x = sx;
+	y = sy;
+	width = swidth;
+	height = sheight;
+	color = scolor;
+	rows = srows;
+	borderType = sborderType;
+	create();
+}
+
+clickableObject * object;
+void menu::create() {
+	remove();
+
+	object = new clickableObject[rows];	//nustatomos meniu lenteliu pasirinkimai, kad juos butu galima paspasti su mouse ir ivyktu funkcija
+	for (int i = 0; i < rows; i++) {
+		object[i].topLeftX = x;
+		object[i].topLeftY = y + height * i;
+		object[i].bottomRightX = x + width;
+		object[i].bottomRightY = y + height * (i + 1);
 	}
 
-	void langas::create() {
-		remove();
-		int tempy = y;
 
-		fill(color);
+	int tempy = y;
+	fill(color);
 
-		char v, b, vk, vd, ak, ad;
+	char v, b, vk, vd, ak, ad;
+	if (borderType == 0) {//jei krastu nera
+		for (int r = 0; r < rows; r++) {
+			lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
+			cout << text[r];
+		}
+	}
+	else {
 		if (borderType == 1) {//viengubas krastas
 			v = char(196);
 			b = char(179);
@@ -291,351 +521,123 @@ namespace lib {
 			ad = char(188);
 		}
 
-		if (borderType != 0) { // jei krastu nera toliau vnieko nereikia daryti
-			lib::setCursorPosition(x, y);
-			for (int i = 0; i <= width; i++) {	//virsus
-				cout << v;
-			}
 
-			tempy = y;
+		lib::setCursorPosition(x, y);
+		for (int i = 0; i <= width; i++) {	//virsus
+			cout << v;
+		}
+
+		tempy = y;
+		for (int r = 0; r < rows; r++) {
 			lib::setCursorPosition(x, tempy + height);
-			for (int i = 0; i < width; i++) {	//apacia
+			for (int i = 0; i <= width; i++) {	//apacia
 				cout << v;
 			}
 
-			for (int i = tempy; i < tempy + height; i++) {	//kaire
+			for (int i = tempy; i <= tempy + height; i++) {	//kaire
 				lib::setCursorPosition(x, i);
 				cout << b;
 			}
 
-			for (int i = tempy; i < tempy + height; i++) {	//desine
+			for (int i = tempy; i <= tempy + height; i++) {	//desine
 				lib::setCursorPosition(x + width, i);
 				cout << b;
 			}
 
 			tempy += height;
-
-			//kampai
-			lib::setCursorPosition(x, y);
-			cout << vk;
-			lib::setCursorPosition(x + width, y);
-			cout << vd;
-			lib::setCursorPosition(x, y + height);
-			cout << ak;
-			lib::setCursorPosition(x + width, y + height);
-			cout << ad;
 		}
 
-	}
+		//kampai
+		lib::setCursorPosition(x, y);
+		cout << vk;
+		lib::setCursorPosition(x + width, y);
+		cout << vd;
+		lib::setCursorPosition(x, y + height * rows);
+		cout << ak;
+		lib::setCursorPosition(x + width, y + height * rows);
+		cout << ad;
 
-	void langas::remove() {
-		lib::setColor(lib::globalColor);
-		int tempy = y;
-
-		//uzspalvinama table vieta
-		lib::setCursorPosition(x, tempy);
-		for (int i = 0; i <= height; i++) {
-			lib::setCursorPosition(x, tempy + i);
-			for (int j = 0; j <= width; j++) {
-				cout << " ";
-			}
-		}
-	}
-
-	void langas::set(int sx, int sy, int swidth, int sheight, int scolor, int sborderType) {
-		/*istrinama sena lentele ir sukuriama nauja lentele su naujais parametrais*/
-		remove();
-		x = sx;
-		y = sy;
-		width = swidth;
-		height = sheight;
-		color = scolor;
-		borderType = sborderType;
-		create();
-	}
-
-
-
-
-	void table::fill(int color) {
-		lib::setColor(color);
-		int tempy = y;
-		//uzspalvinama table vieta/ uzprintinami tusti langeliai kur bus table
+		//tekstas
 		for (int r = 0; r < rows; r++) {
-			lib::setCursorPosition(x, tempy);
-			for (int i = 0; i <= height; i++) {
-				lib::setCursorPosition(x, tempy + i);
-				for (int j = 0; j <= width; j++) {
+			lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
+			cout << text[r];
+		}
+	}
+}
+
+void menu::check() {//tikrinama ar buvo paspausta ar pele uzvesta ant kazkurio is meniu lango
+
+	for (int r = 0; r < rows; r++) {	//tikrinama ant kurio lango uzvesta
+		COORD temp = lib::getMousePosition();	//kai hover pasikeicia spalva
+		if (temp.X > object[r].topLeftX && temp.Y > object[r].topLeftY && object[r].bottomRightX > temp.X && object[r].bottomRightY > temp.Y) {
+			int invertedColor = 255 - color;
+			lib::setColor(invertedColor);
+
+			for (int i = 1; i < height; i++) {//pakeiciama spalva/ hover state
+				lib::setCursorPosition(object[r].topLeftX + 1, object[r].topLeftY + i);
+				for (int j = 1; j < width; j++) {
 					cout << " ";
 				}
 			}
-			tempy += height;
-		}
-	}
+			//kitos spalvos tekstas
+			lib::setCursorPosition(object[r].topLeftX + width / 2 - text[r].length() / 2, object[r].topLeftY + height / 2);
+			cout << text[r];
 
-	void table::create() {	//sukuriama lentele
-		remove();
-		int tempy = y;
-		fill(color);
-
-		char v, b, vk, vd, ak, ad;
-		if (borderType == 0) {	//jei krastu nera tiesiog isvedamas tesktas
-			for (int r = 0; r < rows; r++) {
-				lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
-				cout << text[r];
+			//kol mouse hoverina palikti
+			while (temp.X > object[r].topLeftX && temp.Y > object[r].topLeftY && object[r].bottomRightX > temp.X && object[r].bottomRightY > temp.Y) {
+				temp = lib::getMousePosition();
+				if (lib::mouseLeftClick()) object[r].funkcija(); //jei buvo paspausta ivykdyti funkcija
 			}
-		}
-		else {
-			if (borderType == 1) {	//viengubas krastas
-				v = char(196);
-				b = char(179);
-				vk = char(218);
-				vd = char(191);
-				ak = char(192);
-				ad = char(217);
-			}
-			else if (borderType == 2) {	//dvigubas krastas
-				v = char(205);
-				b = char(186);
-				vk = char(201);
-				vd = char(187);
-				ak = char(200);
-				ad = char(188);
-			}
-
-
-			lib::setCursorPosition(x, y);
-			for (int i = 0; i <= width; i++) {	//virsus
-				cout << v;
-			}
-
-			tempy = y;
-			for (int r = 0; r < rows; r++) {
-				lib::setCursorPosition(x, tempy + height);
-				for (int i = 0; i <= width; i++) {	//apacia
-					cout << v;
-				}
-
-				for (int i = tempy; i <= tempy + height; i++) {	//kaire
-					lib::setCursorPosition(x, i);
-					cout << b;
-				}
-
-				for (int i = tempy; i <= tempy + height; i++) {	//desine
-					lib::setCursorPosition(x + width, i);
-					cout << b;
-				}
-
-				tempy += height;
-			}
-
-			//kampai
-			lib::setCursorPosition(x, y);
-			cout << vk;
-			lib::setCursorPosition(x + width, y);
-			cout << vd;
-			lib::setCursorPosition(x, y + height * rows);
-			cout << ak;
-			lib::setCursorPosition(x + width, y + height * rows);
-			cout << ad;
-
-			//tekstas
-			for (int r = 0; r < rows; r++) {
-				lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
-				cout << text[r];
-			}
-		}
-	}
-
-	void table::remove() {
-		lib::setColor(lib::globalColor);
-		int tempy = y;
-
-		//uzspalvinama table vieta
-		for (int r = 0; r < rows; r++) {
-			lib::setCursorPosition(x, tempy);
-			for (int i = 0; i <= height; i++) {
-				lib::setCursorPosition(x, tempy + i);
-				for (int j = 0; j <= width; j++) {
+			//kai hover nebera istrinamas hover state ir paliekama kaip buvo
+			lib::setColor(color);
+			for (int i = 1; i < height; i++) {
+				lib::setCursorPosition(object[r].topLeftX + 1, object[r].topLeftY + i);
+				for (int j = 1; j < width; j++) {
 					cout << " ";
 				}
 			}
-			tempy += height;
+
+			lib::setCursorPosition(object[r].topLeftX + width / 2 - text[r].length() / 2, object[r].topLeftY + height / 2);
+			cout << text[r];
+
 		}
 	}
+}
 
-	void table::set(int sx, int sy, int swidth, int sheight, int scolor, int srows, int sborderType) {
-		//istrinama sena lentele ir sukuriama nauja lentele su naujais parametrais
-		remove();
-		x = sx;
-		y = sy;
-		width = swidth;
-		height = sheight;
-		color = scolor;
-		rows = srows;
-		borderType = sborderType;
-		create();
-	}
-
-	clickableObject * object;
-	void menu::create() {
-		remove();
-
-		object = new clickableObject[rows];	//nustatomos meniu lenteliu pasirinkimai, kad juos butu galima paspasti su mouse ir ivyktu funkcija
-		for (int i = 0; i < rows; i++) {
-			object[i].topLeftX = x;
-			object[i].topLeftY = y + height * i;
-			object[i].bottomRightX = x + width;
-			object[i].bottomRightY = y + height * (i + 1);
-		}
-
-
-		int tempy = y;
-		fill(color);
-
-		char v, b, vk, vd, ak, ad;
-		if (borderType == 0) {//jei krastu nera
-			for (int r = 0; r < rows; r++) {
-				lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
-				cout << text[r];
-			}
-		}
-		else {
-			if (borderType == 1) {//viengubas krastas
-				v = char(196);
-				b = char(179);
-				vk = char(218);
-				vd = char(191);
-				ak = char(192);
-				ad = char(217);
-			}
-			else if (borderType == 2) {//dvigubas krastas
-				v = char(205);
-				b = char(186);
-				vk = char(201);
-				vd = char(187);
-				ak = char(200);
-				ad = char(188);
-			}
-
-
-			lib::setCursorPosition(x, y);
-			for (int i = 0; i <= width; i++) {	//virsus
-				cout << v;
-			}
-
-			tempy = y;
-			for (int r = 0; r < rows; r++) {
-				lib::setCursorPosition(x, tempy + height);
-				for (int i = 0; i <= width; i++) {	//apacia
-					cout << v;
-				}
-
-				for (int i = tempy; i <= tempy + height; i++) {	//kaire
-					lib::setCursorPosition(x, i);
-					cout << b;
-				}
-
-				for (int i = tempy; i <= tempy + height; i++) {	//desine
-					lib::setCursorPosition(x + width, i);
-					cout << b;
-				}
-
-				tempy += height;
-			}
-
-			//kampai
-			lib::setCursorPosition(x, y);
-			cout << vk;
-			lib::setCursorPosition(x + width, y);
-			cout << vd;
-			lib::setCursorPosition(x, y + height * rows);
-			cout << ak;
-			lib::setCursorPosition(x + width, y + height * rows);
-			cout << ad;
-
-			//tekstas
-			for (int r = 0; r < rows; r++) {
-				lib::setCursorPosition(x + width / 2 - text[r].length() / 2, y + height / 2 + height * r);
-				cout << text[r];
-			}
-		}
-	}
-
-	void menu::check() {//tikrinama ar buvo paspausta ar pele uzvesta ant kazkurio is meniu lango
-
-		for (int r = 0; r < rows; r++) {	//tikrinama ant kurio lango uzvesta
-			COORD temp = lib::getMousePosition();	//kai hover pasikeicia spalva
-			if (temp.X > object[r].topLeftX && temp.Y > object[r].topLeftY && object[r].bottomRightX > temp.X && object[r].bottomRightY > temp.Y) {
-				int invertedColor = 255 - color;
-				lib::setColor(invertedColor);
-
-				for (int i = 1; i < height; i++) {//pakeiciama spalva/ hover state
-					lib::setCursorPosition(object[r].topLeftX + 1, object[r].topLeftY + i);
-					for (int j = 1; j < width; j++) {
-						cout << " ";
-					}
-				}
-				//kitos spalvos tekstas
-				lib::setCursorPosition(object[r].topLeftX + width / 2 - text[r].length() / 2, object[r].topLeftY + height / 2);
-				cout << text[r];
-
-				//kol mouse hoverina palikti
-				while (temp.X > object[r].topLeftX && temp.Y > object[r].topLeftY && object[r].bottomRightX > temp.X && object[r].bottomRightY > temp.Y) {
-					temp = lib::getMousePosition();
-					if (lib::mouseLeftClick()) object[r].funkcija(); //jei buvo paspausta ivykdyti funkcija
-				}
-				//kai hover nebera istrinamas hover state ir paliekama kaip buvo
-				lib::setColor(color);
-				for (int i = 1; i < height; i++) {
-					lib::setCursorPosition(object[r].topLeftX + 1, object[r].topLeftY + i);
-					for (int j = 1; j < width; j++) {
-						cout << " ";
-					}
-				}
-
-				lib::setCursorPosition(object[r].topLeftX + width / 2 - text[r].length() / 2, object[r].topLeftY + height / 2);
-				cout << text[r];
-
-			}
-		}
-	}
-
-	void menu::set(int sx, int sy, int swidth, int sheight, int scolor, int srows, int sborderType) {
-		//istrinama sena lentele ir sukuriama nauja lentele su naujais parametrais
-		remove();
-		x = sx;
-		y = sy;
-		width = swidth;
-		height = sheight;
-		color = scolor;
-		rows = srows;
-		borderType = sborderType;
-		create();
-	}
+void menu::set(int sx, int sy, int swidth, int sheight, int scolor, int srows, int sborderType) {
+	//istrinama sena lentele ir sukuriama nauja lentele su naujais parametrais
+	remove();
+	x = sx;
+	y = sy;
+	width = swidth;
+	height = sheight;
+	color = scolor;
+	rows = srows;
+	borderType = sborderType;
+	create();
+}
 
 
 
 
 
-	void variableText::setCoord(int x1, int y1) { coord.X = x1; coord.Y = y1; }
-	void variableText::Message(string msg) { message = msg; }
-	void variableText::setColor(short col) { color = col; }
-	void variableText::setVariable(double dat) { data = dat; }
-	void variableText::setPosition(bool pos) { position = pos; }
-	void variableText::create() { //if position == true isveda pirmiau teksta
-		lib::setColor(color);
-		lib::setCursorPosition(coord.X, coord.Y);
-		if (position) cout << message << data;
-		else cout << data << message;
-	}
-	//set leidzia viska iskarto padaryti
-	void variableText::set(int x1, int y1, string msg, short col, double dat, bool pos) {
-		setCoord(x1, y1);
-		Message(msg);
-		setColor(col);
-		setVariable(dat);
-		setPosition(pos);
-		create();
-	}
-
+void variableText::setCoord(int x1, int y1) { coord.X = x1; coord.Y = y1; }
+void variableText::Message(string msg) { message = msg; }
+void variableText::setColor(short col) { color = col; }
+void variableText::setVariable(double dat) { data = dat; }
+void variableText::setPosition(bool pos) { position = pos; }
+void variableText::create() { //if position == true isveda pirmiau teksta
+	lib::setColor(color);
+	lib::setCursorPosition(coord.X, coord.Y);
+	if (position) cout << message << data;
+	else cout << data << message;
+}
+//set leidzia viska iskarto padaryti
+void variableText::set(int x1, int y1, string msg, short col, double dat, bool pos) {
+	setCoord(x1, y1);
+	Message(msg);
+	setColor(col);
+	setVariable(dat);
+	setPosition(pos);
+	create();
+}
