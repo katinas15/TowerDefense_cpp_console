aeb7b68eabc3a183c52e1b24f8f175b80f44ecd3
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2018-12-01 23:12:01 +0200

    ?

---
+++ TwGame/gameStart.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "Library.h"
+
+using namespace std;
+
+class enemy {
+private:
+	int x, y;
+public:
+	void move(int a, int b) {
+
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
+
