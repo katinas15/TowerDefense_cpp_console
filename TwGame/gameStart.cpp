7c2e4b74d500745e0a74d7bb28ea0a17e9a6f70a
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2018-12-02 18:14:54 +0200

    a

--- TwGame/gameStart.cpp
+++ TwGame/gameStart.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "Library.h"
+#include <thread>
 
 using namespace std;
 
@@ -7,14 +8,30 @@ class enemy {
 private:
 	int x, y;
 public:
-	void move(int a, int b) {
+	void setXY(int a, int b) {
+		x = a; y = b;
+	}
+	void create() {
 
+		lib::setColor(4);
+		lib::setCursorPosition(x, y - 1);
+		cout << " " << char(203) << " ";
+		lib::setCursorPosition(x, y);
+		cout << char(204) << char(206) << char(185);
+		lib::setCursorPosition(x, y + 1);
+		cout << " " << char(202) << " ";
 	}
 };
 
 int main()
 {
-
+	lib::setFontSize(2, 2);
+	lib::setConsoleResolution(1280, 720);
+	lib::remove_scrollbar();
+	enemy a;
+	a.setXY(5, 5);
+	a.create();
+	getchar();
 	return 0;
 }
 
